e30818ae1da22390362dec28fc3aa5c754460ca6
Maximilian Kulikov <kulikov.maximilian@gmail.com>
2022-05-13 17:47:43 +0200

    autoload fix

--- autoload.php
+++ autoload.php
@@ -1,32 +1,41 @@
 <?php declare(strict_types = 1);
 
-return
-    /**
-     *  @param 'kebab' | 'snake' | null $naming
-     *  Specify the file naming convention with the $naming parameter.
-     *  The default is files named identically to the used PHP identifiers.
-     *  Currently, only kebab and snake case transformations are implemented.
-     *  Note that, since class names are case insensitive, the case is determined
-     *  by the class name used, not the name it was defined with.
-     */
-    function (string $namespace, string $directory, ?string $naming = null):  void {
-        spl_autoload_register(function (string $class) use ($namespace, $directory, $naming) {
-            if (!str_starts_with($class, $namespace))
-                return;
+namespace Psi;
 
-            $base     = $directory;
-            $length   = strlen($namespace);
-            $relative = substr($class, $length);
-            $relative = str_replace('\\', '/', $relative);
-            $relative = match ($naming) {
-                'kebab' => strtolower(preg_replace('/([^A-Z\/])([A-Z])/', '$1-$2', $relative) ?? ''),
-                'snake' => strtolower(preg_replace('/([^A-Z\/])([A-Z])/', '$1_$2', $relative) ?? ''),
-                default => $relative
-            };
+/**
+ *  @param string $namespace Trailing \ optional.
+ *  @param string $directory Trailing / optional.
+ *  @param 'kebab' | 'snake' | null $naming
+ *  Specify the file naming convention with the $naming parameter. The default
+ *  is files named identically to the used PHP identifiers. Currently, only
+ *  kebab and snake case transformations are implemented. Note that, since
+ *  class names are case insensitive, the case is determined by the class name
+ *  used, not the name it was defined with.
+ */
+function loader (string $namespace, string $directory, ?string $naming = null):  void {
+    if (!str_ends_with($namespace, '\\'))
+        $namespace .= '\\';
+    if (!str_ends_with($directory, '/'))
+        $directory .= '/';
+    spl_autoload_register(function (string $class) use ($namespace, $directory, $naming) {
+        if (!str_starts_with($class, $namespace))
+            return;
 
-            $file = $base . $relative . '.php';
+        $base     = $directory;
+        $length   = strlen($namespace);
+        $relative = substr($class, $length);
+        $relative = str_replace('\\', '/', $relative);
+        $relative = match ($naming) {
+            'kebab' => strtolower(preg_replace('/([^A-Z\/])([A-Z])/', '$1-$2', $relative) ?? ''),
+            'snake' => strtolower(preg_replace('/([^A-Z\/])([A-Z])/', '$1_$2', $relative) ?? ''),
+            default => $relative
+        };
 
-            if (file_exists($file))
-                require $file;
-        });
-    };
+        $file = $base . $relative . '.php';
+
+        if (file_exists($file))
+            require $file;
+    });
+};
+
+loader('Psi', __DIR__ . '/src', 'kebab');
